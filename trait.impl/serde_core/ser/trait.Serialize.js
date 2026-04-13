41df2e8aa3fa369356dc88af09fbb8ff21fa390b
biface <biface@users.noreply.github.com>
2026-04-13 16:01:25 +0000

    deploy: 580faebc227137b8e11db8872bfa08c09335ed56

---
+++ trait.impl/serde_core/ser/trait.Serialize.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["chrom_rs",[["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"chrom_rs/physics/data/enum.PhysicalData.html\" title=\"enum chrom_rs::physics::data::PhysicalData\">PhysicalData</a>",0],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"chrom_rs/physics/traits/enum.PhysicalQuantity.html\" title=\"enum chrom_rs::physics::traits::PhysicalQuantity\">PhysicalQuantity</a>",0],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"chrom_rs/physics/traits/struct.PhysicalState.html\" title=\"struct chrom_rs::physics::traits::PhysicalState\">PhysicalState</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[982]}
